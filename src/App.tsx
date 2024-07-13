20e2b13f89bfd8b6eeb2f26a7d7b80dd54dffbf2
seifmegahed <seifmegahed@me.com>
2024-07-13 19:07:29 +0300

    tailwind and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Game from "./pages/game";
+
 function App() {
-  return <></>;
+  return (
+    <div className="screen">
+      <Navbar />
+      <Game />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,4 @@
+function Footer() {
+  return <></>;
+}
+export default Footer;
