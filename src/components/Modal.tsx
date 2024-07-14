9de27ccdbd0ec4913843178764c26a4cb22506f2
seifmegahed <seifmegahed@me.com>
2024-07-14 22:18:55 +0300

    icons

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import ReactDOM from "react-dom";
+import CloseIcon from "../icons/CloseIcon";
 
 function Modal(props: {
   children: ReactNode;
@@ -14,10 +15,10 @@ function Modal(props: {
     <div className="absolute bottom-0 top-0 left-0 right-0 flex justify-center items-center bg-black/40 z-[1000]">
       <div className="bg-white shadow-xl rounded-2xl m-5 max-w-[800px] max-h-[600px] w-full h-full flex flex-col justify-center items-center relative">
         <button
-          className="absolute top-0 right-0 m-2 text-2xl text-black"
+          className="absolute top-0 right-0 m-2 text-2xl text-gray-700 hover:bg-black/10 rounded-full p-3"
           onClick={onClose}
         >
-          X
+          <CloseIcon />
         </button>
         {children}
       </div>
