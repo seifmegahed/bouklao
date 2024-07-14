905ab83da8b855a94bde45368ea5e49816f9a5ef
seifmegahed <seifmegahed@me.com>
2024-07-14 15:34:34 +0300

    styling fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,11 @@
-const FOOTER_IMAGE = "https://utfs.io/f/9076bf45-4d29-49ab-9835-4c863f3c63fa-1zbfv.png";
+const FOOTER_IMAGE =
+  "https://utfs.io/f/9076bf45-4d29-49ab-9835-4c863f3c63fa-1zbfv.png";
 
 function Footer() {
-  return <div className="max-h-[20%] h-full p-5">
-    <img src={FOOTER_IMAGE} className="h-full" />
-  </div>;
+  return (
+    <div className="sm:max-h-[20%] p-5 flex sm:justify-start justify-center items-center w-full flex-grow">
+      <img src={FOOTER_IMAGE} className="h-full max-h-[100px]" />
+    </div>
+  );
 }
 export default Footer;
