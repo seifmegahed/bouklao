bddcfb6e09487f0a9c0a76f4ed59f8a1227726f5
seifmegahed <seifmegahed@me.com>
2024-07-21 06:55:08 +0300

    setup path resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
@@ -5,4 +6,9 @@ import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), visualizer()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
