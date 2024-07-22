c8d2ccf7dd607c3154676655a87cf49db7a08e59
seifmegahed <seifmegahed@me.com>
2024-07-22 19:51:22 +0300

    copy right text on website logo above overlay

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,34 @@ const FOOTER_IMAGE =
 
 function Footer() {
   return (
-    <div className="sm:max-h-[20%] p-5 sm:mb-0 mb-16  flex sm:justify-start justify-center items-center w-full">
-      <img src={FOOTER_IMAGE} className="h-full max-h-[100px]" />
-    </div>
+    <>
+      <div className="sm:max-h-[20%] p-5 sm:mb-0 mb-16  flex sm:justify-start justify-center items-center w-full z-30">
+        <img src={FOOTER_IMAGE} className="h-full max-h-[100px]" />
+      </div>
+      <div className="w-full text-center text-xs text-gray-600 flex flex-col sm:py-3 py-10 fixed bottom-0">
+        <span>
+          &copy; {new Date().getFullYear()}{" "}
+          <a
+            className="text-gray-800 hover:underline cursor-pointer"
+            href="https://www.instagram.com/bouklao"
+            target="_blank"
+            rel="noreferrer"
+          >
+            bouklao
+          </a>
+          {". All rights reserved. Built by "}
+          <a
+            className="text-gray-800 hover:underline cursor-pointer"
+            href="mailto:seifmegahed@me.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            seifmegahed
+          </a>
+          {"."}
+        </span>
+      </div>
+    </>
   );
 }
 export default Footer;
